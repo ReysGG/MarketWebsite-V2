aff5e583f0275017d72351a9ce0b22dbc834fa53
ReysGG <davidreysgg@gmail.com>
2026-01-30 17:01:43 +0700

    feat: Introduce banner management with Prisma schema, API routes for upload/delete, and a reusable file upload UI.

--- app/api/upload/banner/route.ts
+++ app/api/upload/banner/route.ts
@@ -1,18 +1,50 @@
+import prisma from '@/lib/prisma';
 import { put } from '@vercel/blob';
+import { request } from 'http';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request): Promise<NextResponse> {
   const formData = await request.formData();
   const file = formData.get('files') as File;
 
-  if (!file) {
-    return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+  try {
+    if (!file) {
+      return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+    }
+
+    const blob = await put(file.name, file, {
+      access: 'public',
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    });
+
+    await prisma.banner.create({
+      data: {
+        image_url: blob.url,
+        target_url: formData.get('target_url') as string,
+        position: 0,
+        is_active: true,
+      }
+    })
+
+    return NextResponse.json(blob);
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to upload file' }, { status: 500 });
+  }
+}
+
+export const DELETE = async (request: Request) => {
+  const url = await request.json();
+  console.log(url);
+
+  try {
+    await prisma.banner.delete({
+      where: {
+        image_url: url.url as string,
+      }
+    })
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to delete banner' }, { status: 500 });
   }
 
-  const blob = await put(file.name, file, {
-    access: 'public',
-    token: process.env.BLOB_READ_WRITE_TOKEN,
-  });
-
-  return NextResponse.json(blob);
+  return NextResponse.json({ message: 'Banner deleted successfully' });
 }
--- components/uploadbanner.tsx
+++ components/uploadbanner.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { FileUpload } from "@/components/ui/file-upload";
 import { PutBlobResult } from "@vercel/blob";
+import { Button } from "flowbite-react";
 
 export function FileUploadDemo() {
   const [files, setFiles] = useState<File[]>([]);
@@ -26,9 +27,28 @@ export function FileUploadDemo() {
     setUrl(newBlob.url);
   };
 
+  const handleDelete = async (urlLink: string) => {
+    const response = await fetch("/api/upload/banner", {
+      method: "DELETE",
+      body: JSON.stringify({ url: urlLink }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    setUrl("");
+    setFiles([]);
+  }
+
   return (
     <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
       <FileUpload onChange={handleFileUpload} />
+      {url && (
+        <div className="w-full flex justify-center gap-4">
+          <Button className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600" onClick={() => handleDelete(url)}>Delete</Button>
+          <Button className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600">Finish</Button>
+        </div>
+      )}
     </div>
   );
 }
